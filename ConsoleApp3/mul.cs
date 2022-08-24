85af127846fda8753cfb57995a9617c8672cc04a
etg1 <etg1@BG4WS1370>
2022-08-24 17:44:00 +0530

    Add project files.

---
+++ ConsoleApp3/mul.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace ConsoleAppDemo
+{
+    internal class multiplication
+    {
+        public static void Main()
+        {
+            int a, b, c, mult;
+            Console.Write("First number ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Third number ");
+            c = Convert.ToInt32(Console.ReadLine());
+            mult = a * b * c;
+            Console.WriteLine("Product of three number " + mult);
+        }
+    }
+}
